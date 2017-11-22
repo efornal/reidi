64f3cf5e8ddb5da20ecf21e8a121cb7df5b7f306
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2017-11-22 14:11:58 -0300

    improved email verification

--- app/forms.py
+++ app/forms.py
@@ -5,12 +5,21 @@ from django.contrib.auth.models import User
 import logging
 import dns.resolver, dns.exception
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 
 def validate_email_domain_restriction(value):
     logging.info('Checking email domain in preset domains..')
-    email_domain = value.split('@')[1]
-    valid_domains = ['unl.edu.ar','rectorado.unl.edu.ar']
+    valid_domains = []
+    try:
+        email_domain = value.split('@')[1]
+        valid_domains = settings.VALIDATE_EMAIL_DOMAINS
+    except AttributeError, e:
+        logging.warning("Email domain verification is omitted. %s", e)
+    except Exception, e:
+        logging.warning("Email domain verification failed. %s", e)
+        raise forms.ValidationError(_('email_domain_not_exist'))
+    
     if email_domain not in valid_domains:
         logging.warning("Invalid email domain {}, valid are {}"
                         .format(email_domain, valid_domains))
@@ -20,12 +29,23 @@ def validate_email_domain_restriction(value):
 
 def validate_existence_email_domain(value):
     logging.info('Checking the existence of the email domain..')
+    validate = False
     try:
-        email_domain = value.split('@')[1]
-        results = dns.resolver.query(email_domain, 'MX')
+        validate = settings.VALIDATE_EXISTENCE_EMAIL_DOMAIN
+    except AttributeError, e:
+        logging.warning("Email domain existence is omitted. %s", e)
+        return value
+    
+    try:
+        if validate:
+            email_domain = value.split('@')[1]
+            results = dns.resolver.query(email_domain, 'MX')
     except dns.exception.DNSException, e:
-        logging.warning('Domain %s does not exist.', e)
+        logging.warning('Domain does not exist. %s', e)
         raise forms.ValidationError(_('email_domain_not_exist'))
+    except Exception, e:
+        logging.error('ERROR Exception: %s',e)
+
     return value
 
 
