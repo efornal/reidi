a5d0c3a1a4c591c59740d971f385758e784d9e3d
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2017-11-29 09:44:32 -0300

    corrected dependence on migrations

--- app/migrations/0007_permissions_for_users.py
+++ app/migrations/0007_permissions_for_users.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('app', '0005_create_model_application'),
+        ('app', '0006_create_model_documenttype'),
     ]
 
     operations = [
