85adbe14387a83eeaa5a636a12ccb9e5a093c3f8
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2017-12-01 11:11:30 -0300

    renamed templates of registration process and password change based on django authentication system

---
+++ app/registration.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.utils import translation
+import logging
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.mail import BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignupForm
+from .forms import ResetPasswordForm
+from .forms import DefinePasswordForm
+from .forms import ApplicationForm
+from .models import Domain
+from .models import Area
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.core.exceptions import MultipleObjectsReturned
+from django.core.mail import EmailMessage
+
+
+def register(request):
+    context={}
+    return render(request, 'registration/registration_form.html', context)
+
+def send_activation_email(user, request):
+    current_site = get_current_site(request)
+    message = render_to_string('registration/activation_email.html', {
+        'user': user, 
+        'domain': current_site.domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+    })
+    email_to = ['{}'.format(user.email)]
+    subject = settings.EMAIL_SIGN_UP_SUBJECT
+    email_from = settings.EMAIL_SIGN_UP_FROM
+    email_reply_to = settings.EMAIL_REPLY_TO
+    email = EmailMessage(subject,message, email_from, email_to,reply_to=email_reply_to)
+
+    if subject and message and email_from:
+        try:
+            logging.warning("sending sign up email to {}..".format(email_to))
+            if email.send():
+                return True
+            else:
+                logging.warning('The email could not be sent.')
+        except BadHeaderError as e:
+            logging.error("Invalid header found {}".format(e))
+        except Exception as e:
+            logging.error("Exception error: {}".format(e))
+    
+    return False
+
+
+    
+def complete(request):
+    context = {}
+
+    if request.method != 'POST':
+        logging.warning("Attempt to create account without using post method as it should be")
+        return redirect('login')
+    
+    form = SignupForm(request.POST)
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        context.update({'email': user.email})
+        if send_activation_email(user, request):
+            return render(request, 'registration/registration_complete.html', context)
+        else:
+            user.delete()
+            return render(request, 'registration/registration_error.html', context)
+    else:
+        context.update({'form': form})
+        logging.warning("Invalid user form, errors: %s" % form.errors)
+        return render(request, 'registration/registration_form.html', context)
+
+    
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        logging.warning('Activating {} user with token {}'.format(user, token))
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return render(request, 'registration/activation_complete.html')
+    else:
+        logging.error('Error activating {} user with token {}'.format(user, token))
+        return render(request, 'registration/activate.html')
+
+
+
+    
+
--- app/urls.py
+++ app/urls.py
@@ -2,29 +2,26 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from . import views
+from . import registration
+from . import auth
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login,name='login'),
-    url(r'^logout/message',views.logout_message, name='logout_message'),
+    url(r'^accounts/login/$', auth_views.login,name='login'),
+    url(r'^logout/message', auth.logout_message, name='logout_message'),
     url(r'^logout/$', auth_views.logout,{'next_page': 'logout_message'},name='logout'),
     url(r'^en/$', views.index, name='index'),
-    url(r'^application/new/$',
-        views.application_new,
-        name='application_new'),
-    url(r'^application/create/$',
-        views.application_create,
-        name='application_create'),
-    url(r'^account/forgot_password/$', views.account_forgot_password, name='account_forgot_password'),
-    url(r'^account/reset_password/$', views.account_reset_password, name='account_reset_password'),
-    url(r'^account/define_password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.account_define_password, name='account_define_password'),
-    url(r'^account/activate_password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.account_activate_password, name='account_activate_password'),
-    url(r'^account/signup/$', views.account_sign_up, name='account_sign_up'),
-    url(r'^account/create/$', views.account_create, name='account_create'),
-    url(r'^account/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.account_activate, name='account_activate'),
+    url(r'^application/new/$', views.application_new, name='application_new'),
+    url(r'^application/create/$', views.application_create, name='application_create'),
+
+    url(r'^accounts/password/reset/$', auth.password_reset, name='auth_password_reset'),
+    url(r'^accounts/password/reset/complete/$', auth.password_reset_complete, name='auth_password_reset_complete'),
+    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth.password_reset_confirm, name='auth_password_reset_confirm'),
+    url(r'^accounts/password/change/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth.password_change, name='auth_password_change'),
+
+    url(r'^accounts/register/$', registration.register, name='registration_register'),
+    url(r'^accounts/register/complete/$', registration.complete, name='registration_complete'),
+    url(r'^accounts/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', registration.activate, name='registration_activate'),
     url(r'^lang/(?P<lang>\w+)/$', views.set_language, name='set_language'),
     url(r'^$', views.index, name='index'),
 ]
